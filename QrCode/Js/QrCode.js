3edcaa5b4384e7ce10d09d6db1666f0e1990221b
keshavsoft <nknnkumar@yahoo.com>
2023-08-19 16:17:50 +0530

    keshav

---
+++ QrCode/Js/QrCode.js
@@ -0,0 +1,8 @@
+import { StartFunc as StartFuncAddListeners } from "./AddListeners/EntryFile.js";
+
+let StartFunc = () => {
+    console.log("aaaaaaaaa");
+    StartFuncAddListeners();
+};
+
+StartFunc();
